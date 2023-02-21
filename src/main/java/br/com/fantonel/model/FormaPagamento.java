3f3df7d45923d3ef0d4651ec9ab0559c5569166c
fantonel <fabio.antonelli.ti@gmail.com>
2023-02-21 10:27:50 -0300

    Adição do atributo que indica se está ativa ou não.

---
+++ src/main/java/br/com/fantonel/model/FormaPagamento.java
@@ -0,0 +1,83 @@
+package br.com.fantonel.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "formapagamento")
+public class FormaPagamento implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
+	
+	@NotBlank(message = "Informe a forma de pagamento")
+	@Column(name = "formapagamento", length = 60, nullable = false)
+	private String formaPagamento;
+	
+	@NotNull(message = "Informe se a Forma de Pagamento está ou não ativa")
+	@Column(name = "ativa", nullable = false)
+	private boolean ativa = true;
+	
+	public FormaPagamento() {		
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+
+	public boolean isAtiva() {
+		return ativa;
+	}
+
+	public void setAtiva(boolean ativa) {
+		this.ativa = ativa;
+	}
+
+	@Override
+	public String toString() {
+		return "FormaPagamento [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FormaPagamento other = (FormaPagamento) obj;
+		return Objects.equals(id, other.id);
+	}
+}
