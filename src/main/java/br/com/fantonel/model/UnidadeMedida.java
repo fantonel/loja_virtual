4eb4288d9c983406b48985b2c068c22df56b7d43
fantonel <fabio.antonelli.ti@gmail.com>
2023-03-05 09:59:25 -0300

    Model de Unidade de Medida, necessário a cadastro de Produto.

---
+++ src/main/java/br/com/fantonel/model/UnidadeMedida.java
@@ -0,0 +1,80 @@
+package br.com.fantonel.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "unidademedida")
+public class UnidadeMedida implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
+	@NotBlank(message = "Informe a sigla")
+	@Column(name = "sigla", nullable = false, length = 10)
+	private String sigla;
+	@NotBlank(message = "Informe a descrição")
+	@Column(name = "descricao", nullable = false, length = 60)
+	private String descricao;
+
+	public UnidadeMedida() {
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "UnidadeMedida [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UnidadeMedida other = (UnidadeMedida) obj;
+		return Objects.equals(id, other.id);
+	}
+}
