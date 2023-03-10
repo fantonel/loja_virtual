75100c851674552bcf51edc384b1cb671c7322fb
fantonel <fabio.antonelli.ti@gmail.com>
2023-03-10 20:50:17 -0300

    Criação do model de Produto.

---
+++ src/main/java/br/com/fantonel/model/Produto.java
@@ -0,0 +1,145 @@
+package br.com.fantonel.model;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
+	@Column(name = "nome", nullable = false, length = 120)
+	private String nome;
+	@Column(name = "descricao", nullable = false, length = 2000)
+	private String descricao;
+	@Column(name = "numerovisualizacoes", nullable = false)
+	private Double numeroVisualizacoes;
+	@Column(name = "linkyoutube", nullable = false)
+	private String linkYoutube;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "marca_id", referencedColumnName = "id")
+	private Marca marca;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "produtocategoria", uniqueConstraints = @UniqueConstraint(columnNames = {"produto_id", "categoria_id"},name = "produtocategoria_pk"),
+			   joinColumns = @JoinColumn(name = "produto_id", referencedColumnName = "id", table = "produto", unique = false, foreignKey = @ForeignKey(name = "produtocategoria_fk01", value = ConstraintMode.CONSTRAINT)),
+			   inverseJoinColumns = @JoinColumn(name = "categoria_id", referencedColumnName = "id", table = "categoria", unique = false, foreignKey = @ForeignKey(name = "produtocategoria_fk02", value = ConstraintMode.CONSTRAINT))
+	)
+	private List<Categoria> produtoCategorias;
+	
+	@OneToMany(mappedBy = "produto", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	private List<ProdutoConfiguracao> produtoConfiguracoes;
+
+	public Produto() {
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getNumeroVisualizacoes() {
+		return numeroVisualizacoes;
+	}
+
+	public void setNumeroVisualizacoes(Double numeroVisualizacoes) {
+		this.numeroVisualizacoes = numeroVisualizacoes;
+	}
+
+	public String getLinkYoutube() {
+		return linkYoutube;
+	}
+
+	public void setLinkYoutube(String linkYoutube) {
+		this.linkYoutube = linkYoutube;
+	}
+	
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public List<Categoria> getProdutoCategorias() {
+		return produtoCategorias;
+	}
+
+	public void setProdutoCategorias(List<Categoria> produtoCategorias) {
+		this.produtoCategorias = produtoCategorias;
+	}	
+
+	public List<ProdutoConfiguracao> getProdutoConfiguracoes() {
+		return produtoConfiguracoes;
+	}
+
+	public void setProdutoConfiguracoes(List<ProdutoConfiguracao> produtoConfiguracoes) {
+		this.produtoConfiguracoes = produtoConfiguracoes;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+}
