ebd969346b1d909a28944c20a2af2587580e755b
fantonel <fabio.antonelli.ti@gmail.com>
2023-03-26 11:38:11 -0300

    Pessoa Física/Juridica, @JoinColumn, nullable de false para true. O endereço é vinculado a um ou outro tipo de pessoa.

--- src/main/java/br/com/fantonel/model/Endereco.java
+++ src/main/java/br/com/fantonel/model/Endereco.java
@@ -83,12 +83,12 @@ public class Endereco implements Serializable {
 	
 	@JsonIgnore
 	@ManyToOne(targetEntity = PessoaFisica.class)
-	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(name = "pessoa_fisica_fk01", value = ConstraintMode.CONSTRAINT))
+	@JoinColumn(name = "pessoa_id", nullable = true, foreignKey = @ForeignKey(name = "endereco_fk01", value = ConstraintMode.CONSTRAINT))
 	private PessoaFisica pessoaFisica;
 	
 	@JsonIgnore
 	@ManyToOne(targetEntity = PessoaJuridica.class)
-	@JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(name = "pessoa_juridica_fk01", value = ConstraintMode.CONSTRAINT))
+	@JoinColumn(name = "empresa_id", nullable = true, foreignKey = @ForeignKey(name = "endereco_fk02", value = ConstraintMode.CONSTRAINT))
 	private PessoaJuridica pessoaJuridica;
 
 	public Endereco() {
