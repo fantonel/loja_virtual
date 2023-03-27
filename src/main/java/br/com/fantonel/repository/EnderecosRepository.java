b6688d5a9fdad0139a693d973155af69a6485d0b
fantonel <fabio.antonelli.ti@gmail.com>
2023-03-27 18:47:45 -0300

    Service/Repository de endereço: métodos de busca por pessoa/empresa.

--- src/main/java/br/com/fantonel/repository/EnderecosRepository.java
+++ src/main/java/br/com/fantonel/repository/EnderecosRepository.java
@@ -1,12 +1,25 @@
 package br.com.fantonel.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.fantonel.model.Endereco;
 
 @Repository
 public interface EnderecosRepository extends JpaRepository<Endereco, UUID> {
+	@Query("SELECT ender FROM Endereco ender WHERE ender.id = ?1 AND pessoaFisica.id = ?2 AND tipoendereco='COBRANCA' AND ativo=true")
+	Optional<Endereco> findByEnderecoCobrancaPessoaFisica(UUID enderecoId, UUID pessoaID);
+	
+	@Query("SELECT ender FROM Endereco ender WHERE ender.id = ?1 AND pessoaFisica.id = ?2 AND tipoendereco='ENTREGA' AND ativo=true")
+	Optional<Endereco> findByEnderecoEntregaPessoaFisica(UUID enderecoId, UUID pessoaID);
+	
+	@Query("SELECT ender FROM Endereco ender WHERE id = ?1 AND pessoaJuridica.id = ?2 AND tipoendereco='COBRANCA' AND ativo=true")
+	Optional<Endereco> findByEnderecoCobrancaPessoaJuridica(UUID enderecoId, UUID pessoaID);
+	
+	@Query("SELECT ender FROM Endereco ender WHERE id = ?1 AND pessoaJuridica.id = ?2 AND tipoendereco='ENTREGA' AND ativo=true")
+	Optional<Endereco> findByEnderecoEntregaPessoaJuridica(UUID enderecoId, UUID pessoaID);
 }
--- src/main/java/br/com/fantonel/service/EnderecoService.java
+++ src/main/java/br/com/fantonel/service/EnderecoService.java
@@ -1,5 +1,6 @@
 package br.com.fantonel.service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
@@ -22,5 +23,21 @@ public class EnderecoService {
 	
 	public void deleteById(UUID id) {
 		enderecosRepository.deleteById(id);
+	}
+	
+	public Optional<Endereco> findByEnderecoCobrancaPessoaFisica(UUID enderecoId, UUID pessoaID) {
+		return enderecosRepository.findByEnderecoCobrancaPessoaFisica(enderecoId, pessoaID);
+	}
+	
+	public Optional<Endereco> findByEnderecoEntregaPessoaFisica(UUID enderecoId, UUID pessoaID) {
+		return enderecosRepository.findByEnderecoEntregaPessoaFisica(enderecoId, pessoaID);
+	}
+	
+	public Optional<Endereco> findByEnderecoCobrancaPessoaJuridica(UUID enderecoId, UUID pessoaID) {
+		return enderecosRepository.findByEnderecoCobrancaPessoaJuridica(enderecoId, pessoaID);
+	}
+	
+	public Optional<Endereco> findByEnderecoEntregaPessoaJuridica(UUID enderecoId, UUID pessoaID) {
+		return enderecosRepository.findByEnderecoEntregaPessoaJuridica(enderecoId, pessoaID);
 	}
 }
