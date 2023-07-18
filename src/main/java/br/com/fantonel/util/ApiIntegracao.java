811349cca51ee3e4e43adf4000ed8318c6308c1d
bill_ <fabio.antonelli.ti@gmail.com>
2023-07-18 18:10:46 -0300

    Integração API Melhor Envio, novas URLs sandbox.

--- src/main/java/br/com/fantonel/util/ApiIntegracao.java
+++ src/main/java/br/com/fantonel/util/ApiIntegracao.java
@@ -1,9 +1,23 @@
 package br.com.fantonel.util;
 
 public class ApiIntegracao {
+	public static final String MELHORENVIO_SANDBOX_TOKKEN = "Bearer [Seu Token]";
+	//	
 	public static final String MELHORENVIO_SANDBOX_URL_AUTH = "https://sandbox.melhorenvio.com.br/oauth/token";
+	//Cálculo / Opções disponíveis de frete
 	public static final String MELHORENVIO_SANDBOX_URL_CALCULATE = "https://sandbox.melhorenvio.com.br/api/v2/me/shipment/calculate";
-	public static final String MELHORENVIO_SANDBOX_TOKKEN = "Bearer [seu token]";
+	//Inseir Frete
+	public static final String MELHORENVIO_SANDBOX_URL_CART = "https://sandbox.melhorenvio.com.br/api/v2/me/cart";
+	//Comprar Frete
+	public static final String MELHORENVIO_SANDBOX_URL_CHECKOUT = "https://sandbox.melhorenvio.com.br/api/v2/me/shipment/checkout";
+	//Etiqueta - Geração
+	public static final String MELHORENVIO_SANDBOX_URL_TAG = "https://sandbox.melhorenvio.com.br/api/v2/me/shipment/generate";
+	//Etiqueta - Impressão
+	public static final String MELHORENVIO_SANDBOX_URL_TAG_PRINT = "https://sandbox.melhorenvio.com.br/api/v2/me/shipment/print";
+	//Etiqueta - Visualização
+	public static final String MELHORENVIO_SANDBOX_URL_TAG_PREVIEW = "https://sandbox.melhorenvio.com.br/api/v2/me/shipment/preview";
+
+		
 
 	public ApiIntegracao() {
 	}
