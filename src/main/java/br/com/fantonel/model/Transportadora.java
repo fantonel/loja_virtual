d5803372bdfdca1133a292db1fcff74c085e76ba
bill_ <fabio.antonelli.ti@gmail.com>
2023-07-23 13:50:27 -0300

    Transportadora: Criação do Model.

---
+++ src/main/java/br/com/fantonel/model/Transportadora.java
@@ -0,0 +1,84 @@
+package br.com.fantonel.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "transportadora")
+public class Transportadora implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
+	
+	@Column(name = "ativa", nullable = false)
+	private boolean ativa = true;
+	
+	@ManyToOne(targetEntity = PessoaJuridica.class)
+	@JoinColumn(name = "pessoajuridica_id", nullable = false, foreignKey = @ForeignKey(name = "transportadora_fk01", value = ConstraintMode.CONSTRAINT))
+	private PessoaJuridica pessoaJuridica;
+
+	public Transportadora() {
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public boolean isAtiva() {
+		return ativa;
+	}
+
+	public void setAtiva(boolean ativa) {
+		this.ativa = ativa;
+	}
+
+	public PessoaJuridica getPessoaJuridica() {
+		return pessoaJuridica;
+	}
+
+	public void setPessoaJuridica(PessoaJuridica pessoaJuridica) {
+		this.pessoaJuridica = pessoaJuridica;
+	}
+
+	@Override
+	public String toString() {
+		return "Transportadora [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transportadora other = (Transportadora) obj;
+		return Objects.equals(id, other.id);
+	}
+}
