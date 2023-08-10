1f1172b2100350afc10dcea0fad8eb446b058627
bill_ <fabio.antonelli.ti@gmail.com>
2023-08-10 18:14:25 -0300

    Script Tabela melhorenvio: supressão de colunas sem uso.

---
+++ src/main/java/db/migration/V21__202300807_01.sql
@@ -0,0 +1,3 @@
+ALTER TABLE IF EXISTS melhorenvio DROP COLUMN IF EXISTS melhorenvio_json_consultafrete;
+ALTER TABLE IF EXISTS melhorenvio DROP COLUMN IF EXISTS melhorenvio_json_inserefrete;
+ALTER TABLE IF EXISTS melhorenvio DROP COLUMN IF EXISTS melhorenvio_json_comprafrete;
