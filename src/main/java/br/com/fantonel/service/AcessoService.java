b9ece6748f463bc2fd3d9b7002c49305ab1f088c
bill_ <fabio.antonelli.ti@gmail.com>
2023-11-22 20:29:23 -0300

    Add étodo temAcessoRole, que verifica usuário/role de acesso.

--- src/main/java/br/com/fantonel/service/AcessoService.java
+++ src/main/java/br/com/fantonel/service/AcessoService.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,9 @@ import br.com.fantonel.repository.AcessoRepository;
 public class AcessoService {
 	@Autowired
 	private AcessoRepository acessoRepository;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 	 
 	 public boolean existeUsuarioVinculado(UUID id) {
 		 return acessoRepository.existeUsuarioVinculado(id);
@@ -45,5 +52,17 @@ public class AcessoService {
 	 
 	 public void delete(UUID id) {
 		 acessoRepository.deleteById(id);
+	 }
+	 
+	 public boolean temAcessoRole(String userName, String role) {
+			String sql;
+			sql = "SELECT COUNT(*) = 1 \n";
+			sql+= "  FROM usuario usu INNER JOIN usuarioacessos uac ON (uac.usuario_id = usu.id) \n";
+			sql+= "                   INNER JOIN acesso ace ON (ace.id = uac.acesso_id) \n";
+			sql+= " WHERE LOWER(usu.login) = '"+userName.toLowerCase()+"' AND UPPER(ace.descricao) IN ("+role.toUpperCase()+") \n";
+			sql+= "LIMIT 1";
+			Query qry = entityManager.createNativeQuery(sql);
+			boolean result = Boolean.valueOf(qry.getSingleResult().toString());
+			return result;
 	 }
 }
