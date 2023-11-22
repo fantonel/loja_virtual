37f269d3d1c961fb30fc005e579de780893bcf36
bill_ <fabio.antonelli.ti@gmail.com>
2023-11-21 21:15:50 -0300

    Criação de método para recuperar senha.

--- src/main/java/br/com/fantonel/controller/UsuarioController.java
+++ src/main/java/br/com/fantonel/controller/UsuarioController.java
@@ -1,14 +1,19 @@
 package br.com.fantonel.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fantonel.model.Usuario;
 import br.com.fantonel.service.UsuarioService;
 
 @RestController
@@ -27,5 +32,16 @@ public class UsuarioController {
 			return ResponseEntity.status(HttpStatus.FOUND).body(usuario.get());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
-	}	
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
+	@PostMapping("/recuperarSenha")
+	public ResponseEntity<?> recuperarSenha(@RequestBody @Valid Usuario usuario){
+		var usuLogin = usuarioService.findByLogin(usuario.getLogin());
+		if (!usuLogin.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email não encontrado! Verifique se está correto.");
+		}
+		//		
+		return ResponseEntity.status(HttpStatus.OK).body("A nova senha foi enviada para o seu email. Por favor verifique!");		
+	}
 }
